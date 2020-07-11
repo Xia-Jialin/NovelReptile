3422b23004e3916a37f6af1870eab3ee25151c58
““xjl <18253040259@163.com>
2020-07-11 23:17:03 +0800

    优化了一下代码

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ import requests
 import time
 #import json
 
+def getValueList(url, selector):
+    htmlValue = requests.get(url)
+    htmlValue.encoding = 'gbk'  # 更改字符编码
+    soup = BeautifulSoup(htmlValue.text,'html.parser')
+    result = soup.select(selector)
+    return result
 
-url="http://www.huanyue123.com/book/49/49865/"
-wb_data = requests.get(url)
-wb_data.encoding = 'gbk'  # 更改字符编码
-soup = BeautifulSoup(wb_data.text,'html.parser')
-#print(wb_data.text)
-name = soup.select('li > a')
-f = open("天下第九.txt",'a+',encoding="utf-8")
+def getAllValue(urlList, selector):
+    result = ""
+    for zjName in urlList:
+        nr = getValueList(zjName['href'], selector)
+        result += zjName.text + "\n" + nr[0].text + "\n"
+        print(zjName.text)
+        
+    return result
 
-for zj in name:
-    zj_data=requests.get(zj['href'])
-    zj_data.encoding = 'gbk'
-    soup1=BeautifulSoup(zj_data.text,'html.parser')
-    nr = soup1.select('#htmlContent')
-    f.write(zj.text+"\n"+nr[0].text+"\n")
-    print(zj.text)
-    #print(nr[0].text)
+def saveFile(fileName, fileValue):
+    f = open(fileName,'a+',encoding="utf-8")
+    f.write(fileValue)
+    f.close()
 
-f.close()
+def main():
+    url="http://www.huanyue123.com/book/69/69865/"
+    zjurl = getValueList(url, 'li > a')
+    jzValue = getAllValue(zjurl, '#htmlContent')
+    saveFile("天下第九.txt", jzValue)
+    
+if __name__ == "__main__":
+    main()
+    
